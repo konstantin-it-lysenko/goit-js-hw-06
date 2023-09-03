4e265ecc99697cbb2fb83b25d84671515826c0a4
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-03 21:11:05 +0300

    fixed and edited task10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,6 +8,9 @@ input.addEventListener('input', inputListener);
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destoyBoxes);
 
+boxes.style.display = 'flex';
+boxes.style.flexWrap = 'wrap';
+boxes.style.marginTop = '20px';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -25,7 +28,8 @@ function createBoxes() {
   boxes.innerHTML = '';
   if (amount < 0 || amount > 100) return console.error('Error. Please enter number from 1 to 100');
   for (let i = 0; i < amount; i += 1) {
-    boxes.insertAdjacentHTML('beforeend', `<li class="box" style="background-color: ${getRandomHexColor()}"></li>`)
+    let size = 20;
+    boxes.insertAdjacentHTML('beforeend', `<li class="box" style="background-color: ${getRandomHexColor()}; width: ${size + i * 2}px; height: ${size + i * 2}px; border-radius: 50%; margin: 2px"></li>`)
   }
 }
 
