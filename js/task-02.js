fc713496f9c0c13941f81187bb1538a189e9642f
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-03 21:10:15 +0300

    added task2.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.getElementById('ingredients');
+
+//! 1
+// ingredients.forEach((el) => {
+//   const liEl = document.createElement('li');
+//   liEl.classList.add('item');
+//   liEl.textContent = `${el}`;
+//   listEl.insertAdjacentElement('beforeend', liEl)
+// });
+
+//! 2
+const markup = ingredients.map(el => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = `${el}`
+  return liEl.outerHTML;
+}).join('');
+
+listEl.insertAdjacentHTML('beforeend', markup);
