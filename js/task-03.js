0547cb579aa16273542437cf634ef32dc47d0abe
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-03 21:10:37 +0300

    added task3.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listEl = document.querySelector('.gallery');
+
+const markup = images.map(({ url, alt }) => `<li>
+  <img src="${url}" alt="${alt}" width="250">
+</li>`).join('');
+
+listEl.insertAdjacentHTML('beforeend', markup);
