96018f15e290f14ab3658497d64834a867244fef
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-04 20:08:18 +0300

    Added task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,13 +4,9 @@ const destroyBtn = document.querySelector('button[data-destroy]');
 const boxes = document.getElementById('boxes');
 let amount = 0;
 
-input.addEventListener('input', inputListener);
+input.addEventListener('input', targetInputClickHandler);
 createBtn.addEventListener('click', createBoxes);
 destroyBtn.addEventListener('click', destoyBoxes);
-
-boxes.style.display = 'flex';
-boxes.style.flexWrap = 'wrap';
-boxes.style.marginTop = '20px';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -18,21 +14,25 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
-function inputListener(event) {
+function targetInputClickHandler(event) {
   amount = Number(event.currentTarget.value);
-  console.log('amount', amount);
 }
 
 function createBoxes() {
-  boxes.innerHTML = '';
   if (amount < 0 || amount > 100) return console.error('Error. Please enter number from 1 to 100');
+  let size = 20;
   for (let i = 0; i < amount; i += 1) {
-    let size = 20;
     boxes.insertAdjacentHTML('beforeend', `<li class="box" style="background-color: ${getRandomHexColor()}; width: ${size + i * 2}px; height: ${size + i * 2}px; border-radius: 50%; margin: 2px"></li>`)
   }
 }
 
 function destoyBoxes() {
+  amount = 0;
   boxes.innerHTML = '';
+
+  targetInputCleaner(input);
+}
+
+function targetInputCleaner(inp) {
+  inp.value = '';
 }
