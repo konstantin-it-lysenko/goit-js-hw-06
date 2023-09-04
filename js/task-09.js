9cfdf6071efea2493b3ebb8df74ce755670f9884
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-04 20:08:13 +0300

    Added task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+const colorChangeBtn = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+colorChangeBtn.addEventListener('click', changeColorButtonHandler)
+
+function changeColorButtonHandler() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
 }
